d9ab76dd742b3254ba03b7e0404d0dfde92f4d0f
siva7223ispassword <sivaprakash7223@outlook.com>
2025-04-14 16:52:45 +0530

    StudentApp

---
+++ StudentApp/app/src/main/java/com/college/studentapp/activities/DashboardActivity.kt
@@ -0,0 +1,14 @@
+package com.college.studentapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.college.studentapp.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        // TODO: Fetch attendance data from Firebase and display
+    }
+}
